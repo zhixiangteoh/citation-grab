1979246aa197334711ea6781d8e757f3dfe7a718
Zhixiang <zhixiangteoh@hotmail.com>
2020-08-05 00:47:09 +0800

    update list page to reflect developing

--- src/components/List.js
+++ src/components/List.js
@@ -3,20 +3,23 @@ import React from "react";
 class List extends React.Component {
   render() {
     return (
-      <div className="ui divided list">
-        <div className="item">
-          <div className="content">
-            <div className="header">Result 1</div>
-            Citations
+      <React.Fragment>
+        <h2>This page is under development. Please come back later!</h2>
+        <div className="ui divided list">
+          <div className="item">
+            <div className="content">
+              <div className="header">Result 1</div>
+              Citations
+            </div>
+          </div>
+          <div className="item">
+            <div className="content">
+              <div className="header">Result 2</div>
+              Citations
+            </div>
           </div>
         </div>
-        <div className="item">
-          <div className="content">
-            <div className="header">Result 2</div>
-            Citations
-          </div>
-        </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
